6e47e074a296a2f820c9a9c4fa88af25e723f84e
Andreas Eriksson <dreas94@gmail.com>
2022-05-03 15:48:34 +0200

    Finished the exercise

---
+++ src/main/java/se/lexicon/data/DataStorageImpl.java
@@ -0,0 +1,113 @@
+package se.lexicon.data;
+
+
+import se.lexicon.model.Person;
+import se.lexicon.util.PersonGenerator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+/**
+ * Create implementations for all methods. I have already provided an implementation for the first method *
+ */
+public class DataStorageImpl implements DataStorage {
+
+    private static final DataStorage INSTANCE;
+
+    static
+    {
+        INSTANCE = new DataStorageImpl();
+    }
+
+    private final List<Person> personList;
+
+    private DataStorageImpl()
+    {
+        personList = PersonGenerator.getInstance().generate(1000);
+    }
+
+    static DataStorage getInstance()
+    {
+        return INSTANCE;
+    }
+
+
+    @Override
+    public List<Person> findMany(Predicate<Person> filter)
+    {
+        List<Person> result = new ArrayList<>();
+        for(Person person : personList)
+        {
+            if(filter.test(person))
+            {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Person findOne(Predicate<Person> filter)
+    {
+        for(Person person : personList)
+        {
+            if(filter.test(person))
+            {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String findOneAndMapToString(Predicate<Person> filter, Function<Person, String> personToString)
+    {
+        return personToString.apply(findOne(filter));
+    }
+
+    @Override
+    public List<String> findManyAndMapEachToString(Predicate<Person> filter, Function<Person, String> personToString)
+    {
+        List<Person> searchResult = findMany(filter);
+        List<String> result = new ArrayList<>();
+
+        for(Person person : searchResult)
+        {
+            result.add(personToString.apply(person));
+        }
+
+        return result;
+    }
+
+    @Override
+    public void findAndDo(Predicate<Person> filter, Consumer<Person> consumer)
+    {
+        List<Person> searchResult = findMany(filter);
+
+        for(Person person : searchResult)
+        {
+            consumer.accept(person);
+        }
+    }
+
+    @Override
+    public List<Person> findAndSort(Comparator<Person> comparator)
+    {
+        List<Person> result = personList;
+        result.sort(comparator);
+        return result;
+    }
+
+    @Override
+    public List<Person> findAndSort(Predicate<Person> filter, Comparator<Person> comparator)
+    {
+        List<Person> result = findMany(filter);
+        result.sort(comparator);
+        return result;
+    }
+}
